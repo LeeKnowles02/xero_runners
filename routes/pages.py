48c28b89f80995c47c4f2c132ce5896dc0056632
st10051449 <st10051449@vcconnect.edu.za>
2026-03-16 09:31:33 +0200

    Update the UI to be similar to unleahsed

--- routes/pages.py
+++ routes/pages.py
@@ -5,4 +5,8 @@ from flask import render_template
 def register_pages(app):
     @app.get("/")
     def home():
-        return render_template("index.html")
+        return render_template("index.html", active_page="dashboard")
+
+    @app.get("/settings")
+    def settings():
+        return render_template("settings.html", active_page="settings")
